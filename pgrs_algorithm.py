706607c97231bab8d966cfc093706e2427cb8f5c
EduardoGarrido90 <eduardogarrido90@gmail.com>
2022-08-08 09:06:22 +0200

    RS baseline, seed and bugs

--- pgrs_algorithm.py
+++ pgrs_algorithm.py
@@ -1,6 +1,7 @@
 import pyphi
 import numpy as np
 import itertools
+import random
 
 def return_last_batch(phi_evolution, epsilon, i):
     last_batch = []
@@ -38,26 +39,21 @@ def sample_dimension(D_min, p_omega):
             break
     return dimension
 
-def main(D_min, D_max, epsilon, p_omega, mu, T):
+def main(D_min, D_max, epsilon, p_omega, mu, T, seed):
     print('Random search of matrices')
-    D_min=3
-    D_max=5
-    epsilon=5
-    p_omega = np.array([0.2,0.3,0.5])
-    mu=0.2
+    random.seed(seed)
     best_cm = []
     best_tpm = []
     best_state = []
     best_phi = -1
     individuals = {}
     phi_evolution = {}
-    T = 10
-    states = list(itertools.product([0, 1], repeat=nodes))
     while(i<T):
         print('Iteration ' + str(i))
         k=0
         for k in range(0, epsilon):
             nodes = sample_dimension(D_min, p_omega)
+            states = list(itertools.product([0, 1], repeat=nodes))
             tpm_dim = 2**nodes
             tpm = np.random.randint(0,2,tpm_dim*nodes).reshape((tpm_dim,nodes))
             cm = np.random.randint(0,2,nodes*nodes).reshape((nodes,nodes))
@@ -84,7 +80,7 @@ def main(D_min, D_max, epsilon, p_omega, mu, T):
         print('Updating prior distribution')
         p_omega = update_prior(p_omega, phi_evolution, mu, epsilon, i, D_min)
 
-    print('Random search finished')
+    print('Prior Guided Random search finished')
     print('All individuals')
     print(individuals)
     print('Best result')
@@ -92,6 +88,7 @@ def main(D_min, D_max, epsilon, p_omega, mu, T):
     print('cm: ' + str(best_cm))
     print('tpm: ' + str(best_tpm))
     print('state: ' + str(best_state))
+    return best_phi, best_cm, best_tpm, best_state, individuals, phi_evolution
 
 def test():
     print('It works')
@@ -103,5 +100,6 @@ if __name__ == '__main__':
     p_omega = np.array([0.2,0.3,0.5])
     mu=0.2
     T=20
-    main(D_min, D_max, epsilon, p_omega, mu, T)
+    seed=1
+    main(D_min, D_max, epsilon, p_omega, mu, T, seed)
 
