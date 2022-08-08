2b1deff5b623bc02fd2813654625be9564e5cbeb
EduardoGarrido90 <eduardogarrido90@gmail.com>
2022-08-09 00:03:06 +0200

    Plotting routine needed for experiments

--- plot_results.py
+++ plot_results.py
@@ -17,7 +17,6 @@ def substitute_for_max(results):
         i=i+1
     return new_results
 
-
 def plot_simple_experiment(phi_results):
     plt.plot(phi_results)
     plt.xlabel('iterations')
@@ -25,8 +24,23 @@ def plot_simple_experiment(phi_results):
     plt.title('Best phi over the iterations')
     plt.show()
 
+def plot_mean_and_std_dev(means_1, std_devs_1, means_2, std_devs_2, means_3, std_devs_3):
+    iterations = means_1.shape[0]
+    x = np.linspace(1, iterations, iterations)
+    fig, ax0 = plt.subplots(nrows=1, sharex=True)
+    ax0.errorbar(x, means_1, yerr=std_devs_1, fmt='-o', label='PGRS')
+    ax0.errorbar(x, means_2, yerr=std_devs_2, fmt='-o', label='RS')
+    ax0.errorbar(x, means_3, yerr=std_devs_3, fmt='-o', label='GS')
+    ax0.set_title('Best phi over the iterations')
+    plt.xlabel('iterations')
+    plt.ylabel('phi')
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
     test_array = np.array([1.2, 2.3, 3.2, 2.3, 3.5, 4.2, 2.1, 2.2, 6.7, 1.1, 9.0, 9.0, 10.0, 11.0, 13.0, 12.0, 1.0])
-    print(substitute_for_max(test_array))
-    plot_simple_experiment(substitute_for_max(test_array))
+    #print(substitute_for_max(test_array))
+    means = np.array([2.0,3.0,4.0,3.0,2.0,1.0,5.0,2.0])
+    std_devs = np.array([0.2, 0.3, 0.4, 0.1, 0.3, 0.4, 0.2, 0.4])
+    plot_mean_and_std_dev(means, std_devs, means+1, std_devs-0.1, means-1, std_devs+0.5)
+    #plot_simple_experiment(substitute_for_max(test_array))
