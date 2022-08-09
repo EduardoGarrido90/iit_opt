bc0962a74cf1da599639e849644ce47a00d0c7bb
EduardoGarrido90 <eduardogarrido90@gmail.com>
2022-08-09 17:22:58 +0200

    Grid search of binary matrices

--- grid_search_baseline.py
+++ grid_search_baseline.py
@@ -6,7 +6,7 @@ import random
 def main(D_min, D_max, T, seed, debug=True):
     if debug:
         return None, None, None, None, None, np.random.randint(1, 100, T)
-    print('Random search of matrices')
+    print('Grid search of matrices')
     random.seed(seed)
     best_cm = []
     best_tpm = []
@@ -16,13 +16,26 @@ def main(D_min, D_max, T, seed, debug=True):
     phis = np.zeros(T)
     space_range = D_max - D_min + 1
     iterations_for_dimension = T/float(space_range)
-    nodes_array = np.repeat(np.linspace(D_min, D_max, D_max - D_min + 1), iterations_for_dimension)
+    nodes_array = np.repeat(np.linspace(D_min, D_max, space_range), iterations_for_dimension)
+    tpms = {}
+    for sp in range(space_range):
+        current_dimension = sp + D_min
+        tpm_x = 2**current_dimension
+        tpm_y = current_dimension
+        columns = np.array([np.binary_repr(i, width=2**tpm_x) for i in np.linspace(0, 2**tpm_x, tpm_y*iterations_for_dimension).astype(int)])
+        tpms[current_dimension] = np.array([np.fromstring(" ".join(i), sep=" ") for i in columns]).T
+    dimension_index_tpm = 0
+    previous_node=0
     while(i<T):
         print('Iteration ' + str(i))
         nodes = nodes_array[i]
         states = list(itertools.product([0, 1], repeat=nodes))
         tpm_dim = 2**nodes
-        tpm = np.random.randint(0,2,tpm_dim*nodes).reshape((tpm_dim,nodes))
+        if previous_node != 0 and previous_node != nodes:
+            dimension_index_tpm=0
+        previous_node = nodes
+        tpm = tpms[nodes][:,dimension_index_tpm:dimension_index_tpm+tpm_y]
+        dimension_index_tpm = dimension_index_tpm + tpm_y
         cm = np.random.randint(0,2,nodes*nodes).reshape((nodes,nodes))
         j=0
         best_local_phi = -1
