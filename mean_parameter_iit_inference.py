d0c028a1eaaba18802ebc0ecfe63bd2576a008d8
EduardoGarrido90 <eduardogarrido90@gmail.com>
2022-09-09 14:24:10 +0200

    Infeasible solutions updated

--- mean_parameter_iit_inference.py
+++ mean_parameter_iit_inference.py
@@ -3,7 +3,7 @@ import numpy as np
 import pyphi
 import scipy.stats as stats
 
-def compute_phi(cm, tpm, states):
+def compute_phi(cm, tpm, states, infeasibles):
     j=0
     best_local_phi = 0
     while(j<len(states)):
@@ -12,20 +12,23 @@ def compute_phi(cm, tpm, states):
             if phi > best_local_phi:
                 best_local_phi = phi
         except:
+            infeasibles = infeasibles + 1
             pass
         j=j+1
-    return best_local_phi
+    return best_local_phi, infeasibles
 
 if __name__ == '__main__':
     print('Inferring the mean IIT parameter with different number of nodes')
     print('Testing statistical significance of the means. Can it be considered the same?')
     np.random.seed(1)
-    nodes_A = 3
-    nodes_B = 4
+    nodes_A = 4
+    nodes_B = 5
     tpm_dim_A = 2**nodes_A
     tpm_dim_B = 2**nodes_B
-    population_size_A = 30
-    population_size_B = 30
+    population_size_A = 100
+    population_size_B = 100
+    infeasibles_A = 0
+    infeasibles_B = 0
     print('Population size of number of nodes ' + str(nodes_A) +': ' + str(population_size_A))
     print('Population size of number of nodes ' + str(nodes_B) +': ' + str(population_size_B))
     phis_group_A = np.zeros(population_size_A)
@@ -37,8 +40,8 @@ if __name__ == '__main__':
         cm_B = np.random.randint(0,2,nodes_B*nodes_B).reshape((nodes_B,nodes_B))
         states_A = list(itertools.product([0, 1], repeat=nodes_A))
         states_B = list(itertools.product([0, 1], repeat=nodes_B))
-        phis_group_A[i] = compute_phi(cm_A, tpm_A, states_A)
-        phis_group_B[i] = compute_phi(cm_B, tpm_B, states_B)
+        phis_group_A[i], infeasibles_A = compute_phi(cm_A, tpm_A, states_A, infeasibles_A)
+        phis_group_B[i], infeasibles_B = compute_phi(cm_B, tpm_B, states_B, infeasibles_B)
         print('Number ' + str(i) + ' of the population computed.')
     print('IIT computation finished')
     print('Results')
@@ -55,7 +58,7 @@ if __name__ == '__main__':
     ci_width_B = 1.96 * emp_std_dev_B / np.sqrt(population_size_B)
 
     print('Performing two sample t-test. Null hypothesis m1=m2, Alt m1!=m2.')
-    stats.ttest_ind(a=phis_group_A, b=phis_group_B)
+    print(stats.ttest_ind(a=phis_group_A, b=phis_group_B))
 
     print('Classic 95% CI for the mean parameter')
     print('Mean for ' + str(nodes_A) + ' nodes: ' + str(emp_mean_A))
@@ -65,8 +68,7 @@ if __name__ == '__main__':
 
 
     print('Infeasible solutions')
-    infeasible_A = len(np.where(phis_group_A==0)[0])
-    infeasible_B = len(np.where(phis_group_B==0)[0])
-    print('Infeasible solutions found for ' + str(nodes_A) + ' nodes: ' + str((infeasible_A/population_size_A)*100) + '%.')
-    print('Infeasible solutions found for ' + str(nodes_B) + ' nodes: ' + str((infeasible_B/population_size_B)*100) + '%.')
-
+    infeasible_A = len(np.where(phis_group_A==0)[0]) + infeasibles_A
+    infeasible_B = len(np.where(phis_group_B==0)[0]) + infeasibles_B
+    print('Infeasible solutions found for ' + str(nodes_A) + ' nodes: ' + str((infeasibles_A/(population_size_A+infeasibles_A))*100) + '%.')
+    print('Infeasible solutions found for ' + str(nodes_B) + ' nodes: ' + str((infeasibles_B/(population_size_B+infeasibles_B))*100) + '%.')
